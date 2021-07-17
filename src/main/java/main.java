3e6517bc9378de2a112685ea4d70bfbf94bd5008
Duc Huy Nguyen <stdcnguy>
2021-07-17 23:45:09 +0200

    added cli

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,9 +1,24 @@
 import solver.WordJumpSolver;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) throws IOException {
-        WordJumpSolver.jumpToWord("tilt", "well");
+        Scanner sc = new Scanner(System.in);
+        String origin, destination;
+        System.out.println("Input '-1' to exit or press enter to start ");
+        while(!sc.nextLine().equalsIgnoreCase("-1")){
+            System.out.println("Input origin");
+            origin = sc.nextLine();
+            System.out.println("Input destination");
+            destination = sc.nextLine();
+            System.out.println("Solution: ");
+            WordJumpSolver.jumpToWord(origin, destination);
+            System.out.println("Press Enter to continue");
+        }
+
+
+
     }
 }
